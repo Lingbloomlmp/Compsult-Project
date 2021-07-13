fef1f73d771257aceefe94726bbd7c4c616a94d4
JoshuaLingbloom <Lingbloomlmp@gmail.com>
2021-07-13 10:30:25 -0700

    first update

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {}) 
-// const getData =() => {
-//     fetch('https://complimentr.com/api')
-//         .then(response => response.json())
-//         .then(data => renderData(data))
-//         .catch(error => console.log("Error: ", error))
-//     }
-const getData =() => {
+const comData =() => {
+    fetch('https://complimentr.com/api')
+        .then(response => response.json())
+        .then(data => comBtn(data))
+        .catch(error => console.log("Error: ", error))
+}
+const insData =() => {
     fetch('https://evilinsult.com/generate_insult.php?lang=en&type=json')
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(data => insBtn(data))
         .catch(error => console.log("Error: ", error))
-    }
-const btn = () => {
-   let button = document.querySelector('button')
+}
+const insBtn = (data) => {
+   let button = document.querySelector('.badBtn')
     button.addEventListener('click', (e)=>{
        e.preventDefault()
-       renderData()
-    //    console.log(renderData)
+       renderData(data)
     } )
 }
-btn()
-const renderData = (data) =>{
-    console.log(data)   
+const comBtn = (data) => {
+    let button = document.querySelector('.goodBtn')
+     button.addEventListener('click', (e)=>{
+        e.preventDefault()
+        renderData(data)
+     }
+}
+const renderData = (data) => {
+    for(sentence of data){
+        
+    }
 }
