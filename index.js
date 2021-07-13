8ce8069505374637123de263c66d294b854744ab
Franciscoandrade413 <francsicoandrade2001@gmail.com>
2021-07-13 15:32:22 -0700

    db.json created

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-document.addEventListener('DOMContentLoaded', () => {}) 
+document.addEventListener('DOMContentLoaded', () => {
+
+}) 
+
+
 const comData =() => {
     fetch('https://complimentr.com/api')
         .then(response => response.json())
         .then(data => comBtn(data))
         .catch(error => console.log("Error: ", error))
 }
+
+
 const insData =() => {
     fetch('https://evilinsult.com/generate_insult.php?lang=en&type=json')
         .then(response => response.json())
         .then(data => insBtn(data))
         .catch(error => console.log("Error: ", error))
 }
+
+
 const insBtn = (data) => {
    let button = document.querySelector('.badBtn')
     button.addEventListener('click', (e)=>{
@@ -18,13 +26,16 @@ const insBtn = (data) => {
        renderData(data)
     } )
 }
+
+
 const comBtn = (data) => {
     let button = document.querySelector('.goodBtn')
-     button.addEventListener('click', (e)=>{
+    button.addEventListener('click', (e) => {
         e.preventDefault()
         renderData(data)
-     }
+    })
 }
+
 const renderData = (data) => {
     for(sentence of data){
         
