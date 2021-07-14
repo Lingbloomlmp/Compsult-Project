ce0c59397abfff17e1c44b30c1a0feaec1ff7c67
JoshuaLingbloom <Lingbloomlmp@gmail.com>
2021-07-14 15:27:46 -0700

    working compliment

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
-document.addEventListener('DOMContentLoaded', () => {}) 
+document.addEventListener('DOMContentLoaded', () => {comData()}) 
 const comData =() => {
     fetch('https://complimentr.com/api')
         .then(response => response.json())
-        .then(data => comBtn(data))
+        .then(data => compliment(data))
         .catch(error => console.log("Error: ", error))
 }
-const insData =() => {
-    fetch('https://evilinsult.com/generate_insult.php?lang=en&type=json')
-        .then(response => response.json())
-        .then(data => insBtn(data))
-        .catch(error => console.log("Error: ", error))
+const compliment = (data) => {
+    com = Object.values(data)
+    let newCom = document.querySelector('.Compliment')
+    newCom.innerHTML = com[0]
+    console.log(com[0])
 }
-const insBtn = (data) => {
-   let button = document.querySelector('.badBtn')
-    button.addEventListener('click', (e)=>{
-       e.preventDefault()
-       renderData(data)
-    } )
-}
-const comBtn = (data) => {
-    let button = document.querySelector('.goodBtn')
-     button.addEventListener('click', (e)=>{
-        e.preventDefault()
-        renderData(data)
-     }
-}
-const renderData = (data) => {
-    for(sentence of data){
-        
-    }
-}
+
+
+// const insData =() => {
+//     fetch('https://evilinsult.com/generate_insult.php?lang=en&type=json')
+//         .then(response => response.json())
+//         .then(data => insBtn(data))
+//         .catch(error => console.log("Error: ", error))
+// }
+// const insBtn = (data) => {
+//    let button = document.querySelector('.badBtn')
+//     button.addEventListener('click', (e)=>{
+//        e.preventDefault()
+//        renderData(data)
+//     } )
+// }
+// const comBtn = (data) => {
+//     let button = document.querySelector('.goodBtn')
+//      button.addEventListener('click', (e)=>{
+//         e.preventDefault()
+//         renderData(data)
+//      })
+// }
+// const renderData = (data) => {
+//     for(sentence of data){
+//         console.log(data)
+//     }
+// }
